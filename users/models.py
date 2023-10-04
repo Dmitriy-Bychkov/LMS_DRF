3369c923ac17ecf6975a9271b0ebf30d7289a251
Dmitriy Bychkov <582620@gmail.com>
2023-10-04 18:04:34 +0800

    Add new Roles field to the User model

--- users/models.py
+++ users/models.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from education.models import NULLABLE
+
+
+class UserRoles(models.TextChoices):
+    """ Класс на создание ролей рользователя """
+
+    MEMBER = 'member', _('member')
+    MODERATOR = 'moderator', _('moderator')
 
 
 class User(AbstractUser):
@@ -13,6 +21,7 @@ class User(AbstractUser):
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
     country = models.CharField(max_length=50, verbose_name='страна', **NULLABLE)
+    role = models.CharField(max_length=9, choices=UserRoles.choices, default=UserRoles.MEMBER)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
