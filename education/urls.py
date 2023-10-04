13d871cb083fd74dc8b4c0b729e5a7b636ba81e9
Dmitriy Bychkov <582620@gmail.com>
2023-10-04 18:11:10 +0800

    Add url for create payments

--- education/urls.py
+++ education/urls.py
@@ -4,7 +4,7 @@ from education.apps import EducationConfig
 from rest_framework.routers import DefaultRouter
 
 from education.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, \
-    LessonUpdateAPIView, LessonDestroyAPIView, PaymentsListAPIView, PaymentsRetrieveAPIView
+    LessonUpdateAPIView, LessonDestroyAPIView, PaymentsListAPIView, PaymentsRetrieveAPIView, PaymentsCreateAPIView
 
 app_name = EducationConfig.name
 
@@ -18,6 +18,7 @@ urlpatterns = [
     path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
     path('lesson/delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
 
+    path('payments/create/', PaymentsCreateAPIView.as_view(), name='payments_create'),
     path('payments/', PaymentsListAPIView.as_view(), name='payments_list'),
     path('payments/<int:pk>/', PaymentsRetrieveAPIView.as_view(), name='payments_get'),
 ] + router.urls
