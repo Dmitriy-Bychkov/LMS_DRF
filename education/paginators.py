83e569d0db1a88ffdfc995cfec7c00154ba84fee
Dmitriy Bychkov <582620@gmail.com>
2023-10-10 08:19:52 +0800

    Add paginator for preview only 10 items on the page

--- education/paginators.py
+++ education/paginators.py
@@ -5,3 +5,5 @@ class EducationPaginator(PageNumberPagination):
     """ Пагинатор для вывода информации на странице по 10 записей """
 
     page_size = 10
+    page_size_query_param = 'per_page'
+    max_page_size = 100
