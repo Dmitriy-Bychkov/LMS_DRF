8d76c937e09eccd5d2293065016b69c37822c66c
Dmitriy Bychkov <582620@gmail.com>
2023-10-19 21:54:07 +0800

    Make some small changes in a variables for the best understanding of sending process

--- education/tasks.py
+++ education/tasks.py
@@ -15,13 +15,13 @@ def subscriber_notify(course_id):
 
     # если подписки существуют, отправляем подписчикам курса сообщение об изменениях
     if subscriptions:
-        recipient_list = []
+        #recipient_list = []
 
         for subscription in subscriptions:
             message = (
                 f'Уважаемый подписчик, {subscription.user}!\nВ курсе "{course.name}" произошли недавние обновления некоторых уроков.\n'
                 f'Скорее посетите наш сайт, чтобы посмотреть что изменилось в курсе!')
-            recipient_list.append(subscription.user.email)
+            recipient_email = subscription.user.email
 
-        # Вызываем функцию отправки сообщения на email подписчика
-        send_mail_task(subject, message, recipient_list)
+            # Вызываем функцию отправки сообщения на email подписчика
+            send_mail_task(subject, message, [recipient_email])
